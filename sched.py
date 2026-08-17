79730be97bed75537ba714bca9dbbc861881451e
F. Ozge Ozturk <ozozge@gmail.com>
2026-08-17 06:31:34 -0400

    Update TARGET_TWEET_URL to new domain

--- sched.py
+++ sched.py
@@ -19,7 +19,7 @@ auth_token = os.getenv("AUTH_TOKEN")
 anthropic_key = os.getenv("ANTHROPIC_KEY")
 
 # --- Change this URL biweekly to point at the tweet with the current schedule ---
-TARGET_TWEET_URL = "https://twitter.com/DomundiTV/status/2089197270038392898"
+TARGET_TWEET_URL = "https://x.com/DomundiTV/status/2089197270038392898"
 
 name_corrections = {
     "Tie": "Tle",
